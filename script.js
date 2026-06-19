27266630cd1909b0fb7d92892fcb6914950776f2
anusharanikattam <165913529+anusharanikattam@users.noreply.github.com>
2026-06-19 19:14:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+body {
+font-family: Arial;
+margin: 0;
+padding: 0;
+}
+
+header {
+background: #333;
+color: white;
+padding: 15px;
+display: flex;
+justify-content: space-between;
+}
+
+nav a {
+color: white;
+margin: 0 10px;
+text-decoration: none;
+}
+
+.hero {
+text-align: center;
+padding: 100px;
+background: lightblue;
+}
+
+.event {
+margin: 20px;
+padding: 20px;
+border: 1px solid #ccc;
+}
